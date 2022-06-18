b98629ddd23fdfea976116a5373969d9a9fca574
Denis Semenov <Denissemenov42ru@gmail.com>
2022-06-18 19:43:22 +0700

    Practice 001

---
+++ Practice/Practice001/Program.cs
@@ -0,0 +1,33 @@
+﻿int quad, num;
+Console.Write("Введите число : ");
+num = Convert.ToInt32(Console.ReadLine());
+quad = num * num;
+Console.WriteLine("Квадрат вашего числа равен: " + quad);
+
+
+//Задача №3
+int num1, result;
+Console.Write("Введите трехзначное число : ");
+num1 = Convert.ToInt32(Console.ReadLine());
+result = num1 % 10;
+
+Console.WriteLine("Последнее число это :" + result);
+
+//Вывод чисел
+
+int current;
+Console.Write("Введите число : ");
+ int number = Convert.ToInt32(Console.ReadLine());
+
+ if (number < 0)
+ {
+    number = number * (-1);
+ }
+
+current = number * (-1);
+
+while(current <= number)
+{
+    Console.Write(current + " ");
+    current++;
+} 
