91846e5fcca1dacd62dbd520cf64f86a97e37529
Denis Semenov <Denissemenov42ru@gmail.com>
2022-07-05 20:43:52 +0700

    HOMEW ORKE 4

---
+++ HomeWorke/HomeWorke4/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+int NumberSum(int num)
+{
+    int sum = 0;
+
+    while(num > 0){
+        int i = num % 10;
+        num = num / 10;
+        sum = sum + i;
+    }
+    return sum;
+}
+
+Console.Write("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumOfNumbers = NumberSum(num);
+Console.WriteLine("Сумма чисел = " + SumOfNumbers);
+
+
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] NewArray(){
+
+    int num = new Random().Next(0, 9);
+    int[] Array = new int[num];
+    
+    for(int i = 0; i < Array.Length; i++){
+        Array[i] = new Random().Next(0, 100);
+        Console.Write( Array[i] + " ");
+    }
+    return Array;
+}
+
+    int[] arr = NewArray();
+
+void PrintArray(int[] arr)
+{
+    
+ Console.WriteLine("[{0}]", string.Join(", ", arr));
+}
+PrintArray(arr);
+
+
+    
+
+
+
